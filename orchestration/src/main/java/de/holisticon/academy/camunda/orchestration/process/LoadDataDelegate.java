a60505742f7d6bb1ee61064ecded1e5c95176b72
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-11-13 16:28:46 +0100

    Added empty delegate

---
+++ orchestration/src/main/java/de/holisticon/academy/camunda/orchestration/process/LoadDataDelegate.java
@@ -0,0 +1,17 @@
+package de.holisticon.academy.camunda.orchestration.process;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoadDataDelegate implements JavaDelegate {
+
+  private static final Logger logger = LoggerFactory.getLogger(LoadDataDelegate.class);
+
+  public void execute(DelegateExecution execution) {
+    logger.info("Executed by process instance {}", execution.getProcessInstanceId());
+  }
+}
