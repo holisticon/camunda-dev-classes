a4ba82bc042a7a812523f9134c80951c6e6e3450
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-11-15 14:36:47 +0100

    Added empty test

--- orchestration/src/test/java/de/holisticon/academy/camunda/orchestration/process/SimpleDataProcessingTest.java
+++ orchestration/src/test/java/de/holisticon/academy/camunda/orchestration/process/SimpleDataProcessingTest.java
@@ -1,14 +1,17 @@
 package de.holisticon.academy.camunda.orchestration.process;
 
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.spring.boot.starter.test.helper.ProcessEngineRuleRunner;
 import org.camunda.bpm.spring.boot.starter.test.helper.StandaloneInMemoryTestConfiguration;
 import org.junit.Before;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.init;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareAssertions.assertThat;
 
 @RunWith(ProcessEngineRuleRunner.class)
 @Deployment(resources = "simple-data-processing.bpmn")
@@ -25,4 +28,9 @@ public class SimpleDataProcessingTest {
     init(engine.getProcessEngine());
   }
 
+  @Test
+  public void shouldDeploy() {
+    // no asserts, deployment would throw exception and fail the test on errors
+  }
+  
 }
