aab8c376d8f05ae7a01cfbecce9926b0a12d6d32
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-12-03 17:15:22 +0100

    feat: implemented solution

--- choreography/src/main/java/de/holisticon/academy/camunda/choreography/delegate/DeliverPizzaDelegate.java
+++ choreography/src/main/java/de/holisticon/academy/camunda/choreography/delegate/DeliverPizzaDelegate.java
@@ -1,14 +1,35 @@
 package de.holisticon.academy.camunda.choreography.delegate;
 
+import de.holisticon.academy.camunda.choreography.PizzaOrderProcess;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component(value = "deliverPizzaDelegate")
 public class DeliverPizzaDelegate implements JavaDelegate {
 
+  private final RuntimeService runtimeService;
+
+  public DeliverPizzaDelegate(RuntimeService runtimeService) {
+    this.runtimeService = runtimeService;
+  }
+
   @Override
   public void execute(DelegateExecution delegateExecution) {
-    // TODO implement message correlation
+    Optional
+      .ofNullable(
+        this.runtimeService.createExecutionQuery()
+          .processInstanceBusinessKey(delegateExecution.getProcessBusinessKey())
+          .messageEventSubscriptionName(PizzaOrderProcess.Expressions.MESSAGE_PIZZA_RECEIVED)
+          .singleResult()
+      ).ifPresent(e ->
+      this.runtimeService.createMessageCorrelation(PizzaOrderProcess.Expressions.MESSAGE_PIZZA_RECEIVED)
+        .processInstanceId(e.getProcessInstanceId())
+        .setVariable(PizzaOrderProcess.Variables.DELIVERED, true)
+        .correlate()
+    );
   }
 }
--- choreography/src/main/java/de/holisticon/academy/camunda/choreography/delegate/PlaceOrderDelegate.java
+++ choreography/src/main/java/de/holisticon/academy/camunda/choreography/delegate/PlaceOrderDelegate.java
@@ -1,14 +1,31 @@
 package de.holisticon.academy.camunda.choreography.delegate;
 
+import de.holisticon.academy.camunda.choreography.PizzaDeliveryProcess;
+import de.holisticon.academy.camunda.choreography.PizzaOrderProcess;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.stereotype.Component;
 
 @Component(value = "placeOrderDelegate")
 public class PlaceOrderDelegate implements JavaDelegate {
 
+  private final RuntimeService runtimeService;
+
+  public PlaceOrderDelegate(RuntimeService runtimeService) {
+    this.runtimeService = runtimeService;
+  }
+
   @Override
   public void execute(DelegateExecution delegateExecution) {
-    // TODO implement message correlation
+
+    this.runtimeService.correlateMessage(PizzaDeliveryProcess.Expressions.MESSAGE_PLACE_ORDER,
+      delegateExecution.getProcessBusinessKey(),
+      Variables.putValue(PizzaOrderProcess.Variables.SIZE, delegateExecution.getVariable(PizzaOrderProcess.Variables.SIZE))
+        .putValue(PizzaOrderProcess.Variables.TYPE, delegateExecution.getVariable(PizzaOrderProcess.Variables.TYPE))
+        .putValue(PizzaOrderProcess.Variables.DELIVERED, delegateExecution.getVariable(PizzaOrderProcess.Variables.DELIVERED))
+    );
+
   }
 }
