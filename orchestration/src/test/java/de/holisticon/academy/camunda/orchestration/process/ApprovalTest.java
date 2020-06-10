a9ddea93bc9a01a52a57def4ad58c896a6b55e73
Simon Zambrovski <simon.zambrovski@holisticon.de>
2020-06-10 14:16:27 +0200

    patched to updated test lib

--- orchestration/src/test/java/de/holisticon/academy/camunda/orchestration/process/ApprovalTest.java
+++ orchestration/src/test/java/de/holisticon/academy/camunda/orchestration/process/ApprovalTest.java
@@ -11,34 +11,34 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.camunda.bpm.engine.test.assertions.bpmn.AbstractAssertions.init;
-import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareAssertions.assertThat;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.assertThat;
 
 @RunWith(ProcessEngineRuleRunner.class)
 @Deployment(resources = "approval.bpmn")
 public class ApprovalTest {
 
 
-  @Rule
-  public final ProcessEngineRule engine = new StandaloneInMemoryTestConfiguration().rule();
-  private ApprovalProcessBean processBean;
+    @Rule
+    public final ProcessEngineRule engine = new StandaloneInMemoryTestConfiguration().rule();
+    private ApprovalProcessBean processBean;
 
-  @Before
-  public void before() {
-    this.processBean = new ApprovalProcessBean(this.engine.getRuntimeService());
-    init(engine.getProcessEngine());
-  }
+    @Before
+    public void before() {
+        this.processBean = new ApprovalProcessBean(this.engine.getRuntimeService());
+        init(engine.getProcessEngine());
+    }
 
-  @Test
-  public void shouldDeploy() {
-    // no asserts, deployment would throw exception and fail the test on errors
-  }
+    @Test
+    public void shouldDeploy() {
+        // no asserts, deployment would throw exception and fail the test on errors
+    }
 
-  @Test
-  public void shouldStartWaitInApprovalRequested() {
-    ProcessInstance instance = this.processBean.start();
+    @Test
+    public void shouldStartWaitInApprovalRequested() {
+        ProcessInstance instance = this.processBean.start();
 
-    assertThat(instance).isNotNull();
-    assertThat(instance).isWaitingAt(ApprovalProcessBean.Elements.APPROVAL_REQUESTED);
-  }
+        assertThat(instance).isNotNull();
+        assertThat(instance).isWaitingAt(ApprovalProcessBean.Elements.APPROVAL_REQUESTED);
+    }
 
 }
