ed520c15d268e75e7a7cd1a01d6364f5dccefd70
Stefan Zilske <stefan.zilske@holisticon.de>
2025-06-26 14:31:48 +0200

    Fix import

--- choreography/src/main/java/de/holisticon/academy/camunda/choreography/ChoreographyProcessApplication.java
+++ choreography/src/main/java/de/holisticon/academy/camunda/choreography/ChoreographyProcessApplication.java
@@ -1,11 +1,9 @@
 package de.holisticon.academy.camunda.choreography;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
 import org.camunda.bpm.application.ProcessApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @ProcessApplication
 @SpringBootApplication
