e551dcdb21131d3713e7ec252b7c018966e5ac4b
Stefan Zilske <stefan.zilske@holisticon.de>
2025-06-26 14:20:13 +0200

    Add history TTL, remove telemetry

--- choreography/src/main/java/de/holisticon/academy/camunda/choreography/ChoreographyProcessApplication.java
+++ choreography/src/main/java/de/holisticon/academy/camunda/choreography/ChoreographyProcessApplication.java
@@ -1,12 +1,8 @@
 package de.holisticon.academy.camunda.choreography;
 
 import org.camunda.bpm.application.ProcessApplication;
-import org.camunda.bpm.engine.ProcessEngine;
-import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
-import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @ProcessApplication
 @SpringBootApplication
@@ -14,26 +10,6 @@ public class ChoreographyProcessApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ChoreographyProcessApplication.class, args);
-  }
-
-  @Bean
-  ProcessEnginePlugin disableTelemetry() {
-
-    return new ProcessEnginePlugin() {
-      @Override
-      public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
-        processEngineConfiguration.setTelemetryReporterActivate(false);
-        processEngineConfiguration.setInitializeTelemetry(false);
-      }
-
-      @Override
-      public void postInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
-      }
-
-      @Override
-      public void postProcessEngineBuild(ProcessEngine processEngine) {
-      }
-    };
   }
 
 }
