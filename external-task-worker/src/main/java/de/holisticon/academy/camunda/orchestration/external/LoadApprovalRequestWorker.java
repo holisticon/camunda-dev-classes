635b961966882705657246ddac960f730c642a72
Simon Zambrovski <simon.zambrovski@holisticon.de>
2025-07-02 14:52:04 +0200

    Prepare external task class
    solution class 10 external task
    
    color build
    
    Prepare external task class
    
    Prepare external task class
    
    solution class 10 external task
    
    color build
    
    fix prepapration for class 10 external tasks
    
    specify topic name
    
    adopt changes for external task
    
    fix tests
    
    Prepare external task class
    
    solution class 10 external task
    
    Prepare external task class
    
    solution class 10 external task
    
    Prepare external task class
    
    solution class 10 external task
    
    fix prepapration for class 10 external tasks
    
    specify topic name
    
    Prepare external task class
    
    fix prepapration for class 10 external tasks
    
    adopt test changes

---
+++ external-task-worker/src/main/java/de/holisticon/academy/camunda/orchestration/external/LoadApprovalRequestWorker.java
@@ -0,0 +1,55 @@
+package de.holisticon.academy.camunda.orchestration.external;
+
+import de.holisticon.academy.camunda.orchestration.process.Variables;
+import de.holisticon.academy.camunda.orchestration.service.ApprovalRequestRepository;
+import org.camunda.bpm.client.spring.annotation.ExternalTaskSubscription;
+import org.camunda.bpm.client.task.ExternalTask;
+import org.camunda.bpm.client.task.ExternalTaskHandler;
+import org.camunda.bpm.client.task.ExternalTaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Component
+@ExternalTaskSubscription(
+  topicName = "load-approval-request"
+)
+public class LoadApprovalRequestWorker implements ExternalTaskHandler {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(LoadApprovalRequestWorker.class);
+
+  private final ApprovalRequestRepository approvalRequestRepository;
+
+  public LoadApprovalRequestWorker(ApprovalRequestRepository approvalRequestRepository) {
+    this.approvalRequestRepository = approvalRequestRepository;
+  }
+
+  @Override
+  public void execute(ExternalTask externalTask, ExternalTaskService externalTaskService) {
+
+    String id = externalTask.getVariable(Variables.APPROVAL_ID);
+
+    approvalRequestRepository.findById(id).ifPresentOrElse(
+      approvalRequest -> {
+        externalTaskService.complete(externalTask,
+          Map.of(Variables.REQUEST, approvalRequest)
+        );
+        LOGGER.info("Setting request {} as variable", approvalRequest);
+      },
+      () -> {
+        int retries = Optional.ofNullable(externalTask.getRetries()).orElse(3);
+
+        externalTaskService.handleFailure(
+          externalTask.getId(),
+          "Error while loading approval request",
+          "Could not load request with id \" + id",
+          retries - 1,
+          10_000
+        );
+      }
+    );
+  }
+}
