2569942d12181112400aa4962174c02a6b35f78d
Simon Zambrovski <simon.zambrovski@holisticon.de>
2025-07-02 14:52:04 +0200

    Prepare external task class

---
+++ external-task-worker/src/main/java/de/holisticon/academy/camunda/orchestration/process/Variables.java
@@ -0,0 +1,9 @@
+package de.holisticon.academy.camunda.orchestration.process;
+
+public enum Variables {
+  ;
+
+  // variables goes here
+  public final static String APPROVAL_ID = "approvalId";
+  public final static String REQUEST = "request";
+}
